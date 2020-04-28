04206ed0a498c6fc6380af02eac45c6c110d9961
Ciro Continisio <ciro@unity3d.com>
2020-04-28 19:12:25 +0200

    Input
    Rudimentary work on an input system - seems to work!

---
+++ Assets/Scripts/MonoBehaviours/InputListener.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputListener : MonoBehaviour, GameControls.IGameplayActions
+{
+	[HideInInspector] public Vector2 moveInput;
+	[HideInInspector] public Vector2 mousePositionInput;
+	[HideInInspector] public bool fireInput;
+
+	private Plane groundPlane;
+	private GameControls playerControls;
+
+	private void Start()
+	{
+        groundPlane = new Plane(Vector3.up, Vector3.zero);
+	}
+
+	void OnEnable()
+    {
+		//set this GameObject as a listener for the events coming from the Input System
+		playerControls = new GameControls();
+		playerControls.Gameplay.SetCallbacks(this);
+		playerControls.Enable();
+    }
+
+	void OnDisable()
+    {
+		playerControls.Disable();
+    }
+
+	//------------ Input reading ------------ //
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		moveInput = context.ReadValue<Vector2>();
+	}
+
+	public void OnAim(InputAction.CallbackContext context)
+	{
+		mousePositionInput = context.ReadValue<Vector2>();
+
+		Ray ray = Camera.main.ScreenPointToRay(mousePositionInput);
+
+		float enter = 0.0f;
+		if (groundPlane.Raycast(ray, out enter))
+		{
+			Vector3 hitPoint = ray.GetPoint(enter);
+			
+			mousePositionInput.x = hitPoint.x;
+			mousePositionInput.y = hitPoint.z;
+		}
+	}
+
+	public void OnFire(InputAction.CallbackContext context)
+	{
+		fireInput = context.ReadValueAsButton();
+	}
+}
---
+++ Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerInputSystem : SystemBase
+{
+	private InputListener input;
+
+	protected override void OnCreate()
+	{
+		//retrieve the MonoBehaviour which acts as listener for events from the Input System
+		input = GameObject.FindObjectOfType<InputListener>();
+	}
+
+	protected override void OnUpdate()
+	{
+		//transferring input to local variables
+		float2 moveInput = input.moveInput;
+		float2 mousePositionInput = input.mousePositionInput;
+		bool fireInput = input.fireInput;
+
+		Entities.ForEach((ref PlayerInput playerInput) =>
+		{
+			playerInput.Movement = moveInput;
+			playerInput.Pointer = mousePositionInput;
+			playerInput.Fire = fireInput;
+		}).Run();
+	}
+}
