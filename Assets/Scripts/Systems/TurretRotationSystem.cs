58865f64cc1ebb971fa8edf989d63b264bcccc88
Ciro Continisio <ciro@unity3d.com>
2020-05-04 14:27:24 +0200

    Turret rotation
    It works!!!

--- Assets/Scripts/Systems/TurretRotationSystem.cs
+++ Assets/Scripts/Systems/TurretRotationSystem.cs
@@ -5,13 +5,13 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-[UpdateAfter(typeof(TransformSystemGroup))]
+[UpdateBefore(typeof(TransformSystemGroup))]
 public class TurretRotationSystem : SystemBase
 {
 	protected override void OnUpdate()
 	{
 		Entities
-		.ForEach((ref Rotation rotation, ref Translation translation, in LocalToWorld localToWorld, in Parent parentsLocalToWorld, in TurretInput input) =>
+		.ForEach((ref Rotation rotation, ref Translation translation, in LocalToWorld localToWorld, in Parent parent, in TurretInput input) =>
 		{
 			float3 worldTarget = new float3(input.Target.x, 0f, input.Target.y);
 			float3 worldPos = new float3(localToWorld.Position.x, 0f, localToWorld.Position.z);
@@ -19,9 +19,11 @@ public class TurretRotationSystem : SystemBase
 			//calculate world direction
 			float3 dir = math.normalize(worldTarget - worldPos);
 
+			LocalToWorld parentLTW = GetComponent<LocalToWorld>(parent.Value); //random memory access!
+
 			quaternion worldRot = quaternion.LookRotationSafe(dir, math.up());
-			quaternion localRot = math.mul(math.inverse(new quaternion(localToWorld.Value)), worldRot);
-			//rotation.Value = localRot;
+			quaternion localRot = math.mul(math.inverse(new quaternion(parentLTW.Value)), worldRot);
+			rotation.Value = localRot;
 		}).Schedule();
 	}
 }
