f609faf72e3444e05eb53955722332bb3954cd90
Fabrice Lete <fabricel@unity3d.com>
2020-05-04 19:16:21 +0200

    turret rotation

--- Assets/Scripts/Systems/TurretRotationSystem.cs
+++ Assets/Scripts/Systems/TurretRotationSystem.cs
@@ -11,7 +11,7 @@ public class TurretRotationSystem : SystemBase
 	protected override void OnUpdate()
 	{
 		Entities
-		.ForEach((ref Rotation rotation, ref Translation translation, in LocalToWorld localToWorld, in Parent parentsLocalToWorld, in TurretInput input) =>
+		.ForEach((ref Rotation rotation, ref Translation translation, in LocalToWorld localToWorld, in Parent parent, in TurretInput input) =>
 		{
 			float3 worldTarget = new float3(input.Target.x, 0f, input.Target.y);
 			float3 worldPos = new float3(localToWorld.Position.x, 0f, localToWorld.Position.z);
@@ -20,8 +20,12 @@ public class TurretRotationSystem : SystemBase
 			float3 dir = math.normalize(worldTarget - worldPos);
 
 			quaternion worldRot = quaternion.LookRotationSafe(dir, math.up());
-			quaternion localRot = math.mul(math.inverse(new quaternion(localToWorld.Value)), worldRot);
-			//rotation.Value = localRot;
+
+			var parentLocalToWorld = GetComponent<LocalToWorld>(parent.Value);
+			var parentRot = parentLocalToWorld.Rotation;
+			var localRot = math.mul(math.inverse(parentRot), worldRot);
+
+			rotation.Value = localRot;
 		}).Schedule();
 	}
 }
